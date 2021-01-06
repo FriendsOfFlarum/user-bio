4bb13973f13c4567ef81ec35835327037cd3a6da
Daniël Klabbers <daniel@klabbers.email>
2021-01-06 12:00:55 +0100

    added flarum/suspend integration

--- src/Access/UserPolicy.php
+++ src/Access/UserPolicy.php
@@ -11,6 +11,7 @@
 
 namespace FoF\UserBio\Access;
 
+use Carbon\Carbon;
 use Flarum\User\Access\AbstractPolicy;
 use Flarum\User\User;
 
@@ -18,6 +19,11 @@ class UserPolicy extends AbstractPolicy
 {
     public function viewBio(User $actor, User $user)
     {
+        // Suspended users won't show their Bio, unless you are allowed to edit any bio.
+        if (!$actor->hasPermission('fof-user-bio.editAny') && $this->isSuspended($user)) {
+            return $this->deny();
+        }
+
         // We only let the user see its own bio if they are also allowed to edit it
         if (($actor->id === $user->id && $actor->hasPermission('fof-user-bio.editOwn'))
             || $actor->hasPermission('fof-user-bio.view')
@@ -30,11 +36,23 @@ class UserPolicy extends AbstractPolicy
 
     public function editBio(User $actor, User $user)
     {
-        if (($actor->id === $user->id && $actor->hasPermission('fof-user-bio.editOwn'))
+        if (($actor->id === $user->id
+                && $actor->hasPermission('fof-user-bio.editOwn')
+                && ! $this->isSuspended($user))
             || $actor->hasPermission('fof-user-bio.editAny')) {
             return $this->allow();
         }
 
         return $this->deny();
+    }
+
+    protected function isSuspended(User $user): bool
+    {
+        // suspended_until is null if flarum/suspend isn't installed
+        // laravel sets all non existing attributes to null
+        // suspend_until is also null if the user isn't suspended.
+        return $user->suspended_until !== null
+            && $user->suspended_until instanceof Carbon
+            && $user->suspended_until->isFuture();
     }
 }
