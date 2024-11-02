6af76fbe76b99290bd3585c1b84933238337f752
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2024-11-02 10:43:47 +0100

    chore: minor change

--- js/src/forum/extend.ts
+++ js/src/forum/extend.ts
@@ -4,7 +4,7 @@ import User from 'flarum/common/models/User';
 export default [
   new Extend.Model(User) //
     .attribute<string>('bio')
-    .attribute<string>('bioHtml')
+    .attribute<string | null>('bioHtml')
     .attribute<boolean>('canViewBio')
     .attribute<boolean>('canEditBio'),
 ];
