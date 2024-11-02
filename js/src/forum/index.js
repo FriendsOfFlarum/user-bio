21b1a256f15257e21ab63d511f9a23ea57d032e9
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2024-11-02 09:24:07 +0100

    refactor: use frontend extenders

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -2,16 +2,13 @@ import app from 'flarum/forum/app';
 import { extend } from 'flarum/common/extend';
 import 'autolink-js';
 import UserCard from 'flarum/forum/components/UserCard';
-import User from 'flarum/common/models/User';
-import Model from 'flarum/common/Model';
 import UserBio from './components/UserBio';
 
 export * from './components';
 
-app.initializers.add('fof-user-bio', () => {
-  User.prototype.bio = Model.attribute('bio');
-  User.prototype.bioHtml = Model.attribute('bioHtml');
+export { default as extend } from './extend';
 
+app.initializers.add('fof-user-bio', () => {
   extend(UserCard.prototype, 'infoItems', function (items) {
     let user = this.attrs.user;
 
