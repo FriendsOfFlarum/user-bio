bedc02430553a1742a32d8615c649dccda5330b4
IanM <ian@morland.me>
2025-10-27 06:02:28 +0000

    chore: typing workaround

--- js/src/forum/extenders/extendUserCard.tsx
+++ js/src/forum/extenders/extendUserCard.tsx
@@ -2,16 +2,21 @@ import { extend } from 'flarum/common/extend';
 import ItemList from 'flarum/common/utils/ItemList';
 import UserCard from 'flarum/forum/components/UserCard';
 import UserBio from '../components/UserBio';
+import User from 'flarum/common/models/User';
 import type Mithril from 'mithril';
 
 export default function extendUserCard() {
   extend(UserCard.prototype, 'infoItems', function (items: ItemList<Mithril.Children>) {
-    const user = this.attrs.user;
+    // @TODO: fix typing here after `UserCard` is typed properly
+    // @ts-ignore
+    const user = this.attrs.user as User;
+    // @ts-ignore
+    const canEdit = this.attrs.editable as boolean;
 
     if (!user.attribute('canViewBio')) {
       return;
     }
 
-    items.add('bio', <UserBio user={user} editable={this.attrs.editable} />, -100);
+    items.add('bio', <UserBio user={user} editable={canEdit} />, -100);
   });
 }
